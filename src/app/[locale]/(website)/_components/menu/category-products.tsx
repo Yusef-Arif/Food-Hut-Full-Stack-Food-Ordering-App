32650033f0ed73fc27d42d5732ce8b501dce54a8
yusef arif <yusefarif616@gmail.com>
2025-08-12 19:43:48 +0300

    Fix TypeScript error

--- src/app/[locale]/(website)/_components/menu/category-products.tsx
+++ src/app/[locale]/(website)/_components/menu/category-products.tsx
@@ -49,7 +49,7 @@ const CategoryProducts = ({
       ) : (
         <div>
           <div className="max-md:hidden grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
-            {Category?.product.slice(0, 4).map((product) => (
+            {Category?.product.slice(0, 4).map((product: any) => (
               <div key={product.id}>
                 <ProductCard product={product} translation={translation} />
               </div>
